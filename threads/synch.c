3c75e8d73fde58617948ab4b3c15627a7dd10f88
Hyobeom Jang <93966249+InfraWhale@users.noreply.github.com>
2024-09-29 10:59:51 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -66,7 +66,6 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) { // while문이므로, sema value 0인 동안 대기
-		// list_push_back (&sema->waiters, &thread_current ()->elem);
 		list_insert_ordered(&sema->waiters, &thread_current ()->elem, better_priority, NULL);
 		thread_block ();
 	}
