b8fecbf579b77715e799c98a7a1c2875d9ca493e
Hyobeom Jang <93966249+InfraWhale@users.noreply.github.com>
2024-09-29 11:00:22 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -102,20 +102,6 @@ sema_try_down (struct semaphore *sema) {
    and wakes up one thread of those waiting for SEMA, if any.
 
    This function may be called from an interrupt handler. */
-// void
-// sema_up (struct semaphore *sema) {
-// 	enum intr_level old_level;
-
-// 	ASSERT (sema != NULL);
-
-// 	old_level = intr_disable ();
-// 	if (!list_empty (&sema->waiters))
-// 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
-// 					struct thread, elem));
-// 	sema->value++;
-// 	intr_set_level (old_level);
-// }
-
 void
 sema_up (struct semaphore *sema) {
 	enum intr_level old_level;
