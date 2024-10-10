ba3af66c71d386d0a6690350b681678928fcccbd
Domvy <seonguk8717@gmail.com>
2024-10-10 21:00:57 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -175,21 +175,6 @@ vm_do_claim_page (struct page *page) {
 
 /* Initialize new supplemental page table */
 
-// 예시 구조체
-struct vm_entry {
-    uint8_t type;
-    void *vaddr;
-    bool writable;
-    bool is_loaded;
-    struct file *file;
-    struct list_elem mmap_elem;
-    size_t offset;
-    size_t read_bytes;
-    size_t zero_bytes;
-    size_t swap_slot;
-    struct hash_elem elem;
-};
-
 // 해시 함수
 uint64_t vm_hash_func(const struct hash_elem *e, void *aux) {
     struct vm_entry *i = hash_entry(e, struct vm_entry, elem);
