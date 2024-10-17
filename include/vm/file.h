44ca9cb6db8364adae16d1984e2a4559386a89dc
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-17 06:50:22 +0000

    feat(file-mapped): Add struct file_page member

--- include/vm/file.h
+++ include/vm/file.h
@@ -6,7 +6,11 @@
 struct page;
 enum vm_type;
 
-struct file_page {};
+struct file_page {
+    struct file *file;
+    off_t offset;
+    size_t page_read_bytes;
+};
 
 void vm_file_init( void );
 bool file_backed_initializer( struct page *page, enum vm_type type, void *kva );
